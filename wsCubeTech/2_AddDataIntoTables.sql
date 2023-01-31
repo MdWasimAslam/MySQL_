f1ec45d1bb45841f34c461823af7f9177a07414b
Md Wasim Aslam <wasimaslam2897@gmail.com>
2023-02-01 02:34:58 +0530

    #

--- wsCubeTech/2_AddDataIntoTables.sql
+++ wsCubeTech/2_AddDataIntoTables.sql
@@ -1,7 +1,22 @@
 -- INSERT DATA INTO TABLE
 -- Select Db which contains that table
 
+
+--- INSERT SINGLE DATA INTO TABLE
+
 INSERT into users 
 (id,name,email,password,contact,address,dob,gender,status)
 VALUES 
 (1,"John Doe","john@email.com","pwd123","+9198754755","34,Baker St","1999-01-10","M",1)
+
+
+
+
+--- INSERT MULTIPLE DATA INTO TABLES
+INSERT into users 
+(id,name,email,password,contact,address,dob,gender,status)
+VALUES 
+(2,"lester James","lester@email.com","pwd123","+91986546755","12,Park St","1997-06-09","M",1),
+(3,"Karen Smith","karen@email.com","pwd123","+9123432432","56, Rye St","1923-12-01","M",1),
+(4,"Julius Ceasor","julius@email.com","pwd123","+9194564755","434,polr St","1989-01-11","M",1),
+(5,"Lions Gate","lions@email.com","pwd123","+91987434755","59,Trio St","2021-04-12","M",1)
