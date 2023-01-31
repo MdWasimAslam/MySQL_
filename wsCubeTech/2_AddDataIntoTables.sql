6f264d1ae70b50c1e41d099261abcbc3bb10dc0a
Md Wasim Aslam <wasimaslam2897@gmail.com>
2023-02-01 02:41:11 +0530

    #

--- wsCubeTech/2_AddDataIntoTables.sql
+++ wsCubeTech/2_AddDataIntoTables.sql
@@ -3,15 +3,8 @@
 
 
 
--- INSERT SINGLE DATA (NOT RECOMENDED) 
-INSERT into users 
-VALUES 
-(6,"Orion Jack","orion@email.com","pwd123","+919544755","24,King St","1969-01-10","F",1)
 
-
-
-
---- INSERT SINGLE DATA INTO TABLE (RECOMENDED)
+--- INSERT SINGLE DATA INTO TABLE 
 
 INSERT into users 
 (id,name,email,password,contact,address,dob,gender,status)
@@ -29,3 +22,10 @@ VALUES
 (3,"Karen Smith","karen@email.com","pwd123","+9123432432","56, Rye St","1923-12-01","M",1),
 (4,"Julius Ceasor","julius@email.com","pwd123","+9194564755","434,polr St","1989-01-11","M",1),
 (5,"Lions Gate","lions@email.com","pwd123","+91987434755","59,Trio St","2021-04-12","M",1)
+
+
+
+-- INSERT SINGLE DATA ( use only when you are filling the complete columns of the table) 
+INSERT into users 
+VALUES 
+(6,"Orion Jack","orion@email.com","pwd123","+919544755","24,King St","1969-01-10","F",1)
