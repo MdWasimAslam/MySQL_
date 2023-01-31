6b46d94382fa29cdd865da00b527c5b2371211e3
Md Wasim Aslam <wasimaslam2897@gmail.com>
2023-02-01 02:29:07 +0530

    #

--- wsCubeTech/1_Databases&Tables.sql
+++ wsCubeTech/1_Databases&Tables.sql
@@ -4,3 +4,17 @@ create database wscube;
 -- Selecting that database
 use wscube;
 
+--- CREATING A DEMO TABLE for users
+create table users 
+(
+ id int unsigned,
+ name varchar(100),
+ email varchar(150),
+ password varchar(150),
+ contact varchar(20),
+ address text,
+ dob date,
+ gender enum("M","F","O"),
+ status boolean
+);
+
---
+++ wsCubeTech/2_AddDataIntoTables.sql
@@ -0,0 +1,7 @@
+-- INSERT DATA INTO TABLE
+-- Select Db which contains that table
+
+INSERT into users 
+(id,name,email,password,contact,address,dob,gender,status)
+VALUES 
+(1,"John Doe","john@email.com","pwd123","+9198754755","34,Baker St","1999-01-10","M",1)
