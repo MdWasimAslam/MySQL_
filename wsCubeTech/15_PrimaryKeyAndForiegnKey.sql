228be921a639ed7eb6fb142927fd31bfe7e76c16
Wasi <wasimaslam2897@gmail.com>
2023-02-04 19:05:59 +0530

    #

---
+++ wsCubeTech/15_PrimaryKeyAndForiegnKey.sql
@@ -0,0 +1,8 @@
+-- PRIMARY KEY 
+-- 1. Always stores unique data
+-- 2. It cannot be null
+-- 3. there will be only one primary key
+
+-- FORIEGN KEY 
+-- 1. It is used to link two tables
+-- 2. A foreign key in one table (child ) is used to point primary  key in another table(parent )
