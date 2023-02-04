dff9c75499a2d83829764f7525a0aafaa5f129ec
Wasi <wasimaslam2897@gmail.com>
2023-02-04 17:42:33 +0530

    #

---
+++ wsCubeTech/10_LimitAndOffSet.sql
@@ -0,0 +1,7 @@
+-- Suppose you want to get only specific number of data from table 
+
+-- This will return all data from datable
+select * from users; 
+
+-- This will return only 5 data from database
+select * from users limit 5; 
