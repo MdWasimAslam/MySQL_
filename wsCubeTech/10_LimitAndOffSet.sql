bb3c9ecc6db0de7260018f8ce845ea0c64a8b82e
Wasi <wasimaslam2897@gmail.com>
2023-02-04 17:47:52 +0530

    #

--- wsCubeTech/10_LimitAndOffSet.sql
+++ wsCubeTech/10_LimitAndOffSet.sql
@@ -5,3 +5,6 @@ select * from users;
 
 -- This will return only 5 data from database
 select * from users limit 5; 
+
+-- Get all users in order of descending order of age and get only 5 data 
+select * from users order by age desc limit 5; 
