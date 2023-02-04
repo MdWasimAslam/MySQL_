ea8ddbc0f84adbc4294b4a033e06315fdb15d0ff
Wasi <wasimaslam2897@gmail.com>
2023-02-04 17:27:05 +0530

    #

--- wsCubeTech/7_BetweenAndNotBetweenOperator.sql
+++ wsCubeTech/7_BetweenAndNotBetweenOperator.sql
@@ -1,2 +1,6 @@
 -- Get all users whose age is between 18 to 22
 select * from users where age between 18 AND 22;
+
+
+-- Get all users whose age is not between 18 to 22
+select * from users where age not between 18 AND 22;
