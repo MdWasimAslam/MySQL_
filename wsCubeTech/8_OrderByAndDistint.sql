5e1475cbc454a514f6e533778d179f0dbb571334
Wasi <wasimaslam2897@gmail.com>
2023-02-04 17:30:59 +0530

    #

---
+++ wsCubeTech/8_OrderByAndDistint.sql
@@ -0,0 +1,12 @@
+-- Get all users name in order of assending order
+select * from users
+order by name ASC;
+
+-- Get all users age in order of assending order
+select * from users
+order by age ASC;
+
+-- Get all user age in order of decending order
+select * from users
+order by age DESC;
+
