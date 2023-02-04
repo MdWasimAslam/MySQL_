f50b976118493e92394c6aef81499126d5480c94
Wasi <wasimaslam2897@gmail.com>
2023-02-04 18:24:15 +0530

    #

--- wsCubeTech/10_LimitAndOffSet.sql
+++ wsCubeTech/10_LimitAndOffSet.sql
@@ -8,3 +8,7 @@ select * from users limit 5;
 
 -- Get all users in order of descending order of age and get only 5 data 
 select * from users order by age desc limit 5; 
+
+
+-- Get 10 data from user table but start with third data 
+select * from users limit 10 offset 3;  
