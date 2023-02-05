a0f845df22027374cbe1e07b9fe39b6be04d8fb0
Md Wasim Aslam <wasimaslam2897@gmail.com>
2023-02-05 21:26:35 +0530

    #

--- wsCubeTech/15_PrimaryKeyAndForiegnKey.sql
+++ wsCubeTech/15_PrimaryKeyAndForiegnKey.sql
@@ -6,3 +6,17 @@
 -- FORIEGN KEY 
 -- 1. It is used to link two tables
 -- 2. A foreign key in one table (child ) is used to point primary  key in another table(parent )
+
+
+
+-- Creating a new table with primary key and foreign key 
+-- This table will connect with foreign key of student to primary  key "cid" of cities table
+create table students  (
+id int not null unique auto_increment,
+name varchar (255) not null,
+email varchar(255) not null unique,
+city_id int null,
+primary key(id),
+foreign key (city_id) references cities(cid)
+)
+
